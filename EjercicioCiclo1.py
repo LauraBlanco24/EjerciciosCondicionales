d6d70caa7aae5cd7dc95775646c21abe7767eb59
LauraBlanco24 <laura.blanco01@correo.usa.edu.co>
2020-09-17 19:32:52 -0500

    Cambio en ejercicio uno ciclos

--- EjercicioCiclo1.py
+++ EjercicioCiclo1.py
@@ -1,7 +1,9 @@
 def palindroma(palabra):
-    
-        if palabra== palabra[::-1]:
-            print ("Es palindroma") 
-        else:
-            print("No es palindroma")
+    reves=""
+    for i in reversed(range(0,len(palabra))):
+        reves+=palabra[i]
+    if palabra==reves:
+        print ("Es palindroma")
+    else:
+        print("No es palindroma")
         
